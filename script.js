02368b562a85d4d553b8db0a9075288cadd079d8
bmcilhenny <bmcilhen@gmail.com>
2019-07-16 22:29:06 -0400

    add starter code for gritty scoreboard

--- script.js
+++ script.js
@@ -2,21 +2,11 @@ const main = function() {
   // using querySelectorAll cause they're more modern, they return a Nodelist as opposed to a HTML collection like getElementsByClassName. You cannot call .forEach on a HTML collection BUT can on a Node List. Ugh... DOM unintuitiveness
   const menues = document.querySelectorAll(".more-menu");
   const notificationsBadge = document.getElementById("notifications-badge");
-  // let numberActive = $('.glyphicon-bell.active').length;
-  // wrong spot, it's being declared too early;
-  // we'll add a watch to numberActive to see if it changes, if it does not then we know something is wrong
+  let numberActive = $('.glyphicon-bell.active').length;
   document.addEventListener("click", event => {
     if (event.target.matches(".more-btn")) {
-      //  debugger;
-      //  first step
-      //   console.log(menu)
-      //   second step
-      //   console.log('How many items are being toggled?', menu.length);
-      //  adds breakpoint
-      // debugger;
       menues.forEach(menu => {
         if (getComputedStyle(menu).display === "none") {
-          debugger;
           menu.style.setProperty("display", "block");
         } else {
           menu.style.setProperty("display", "none");
@@ -24,19 +14,14 @@ const main = function() {
       });
     }
 
-    if (event.target.matches(".notification")) {
-      event.target.classList.contains("active")
-        ? event.target.classList.remove("active")
-        : event.target.classList.add("active");
-      console.log("HERE");
-      let numberActive = document.querySelectorAll(".glyphicon-bell.active")
-        .length;
-      // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
-      if (numberActive) {
-        notificationsBadge.textContent = numberActive;
-      } else {
-        notificationsBadge.textContent = "";
-      }
+    if (even.target.matches(".notification")) {
+      // classList is not supported in internet explorer
+      // arr = element.className.split(" ");
+      // if (arr.indexOf(name) == -1) {
+      //   element.className += " " + name;
+      // }
+      event.target.className += "active";
+      notificationsBadge.textContent = numberActive;
     }
   });
 };
