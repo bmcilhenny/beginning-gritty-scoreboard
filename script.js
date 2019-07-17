f15ec84f8ea73778d57e80cd5f432c32d04eec70
bmcilhenny <bmcilhen@gmail.com>
2019-07-16 22:56:43 -0400

    replace jquery syntax with modern js queryselectorAll

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const main = function() {
   // using querySelectorAll cause they're more modern, they return a Nodelist as opposed to a HTML collection like getElementsByClassName. You cannot call .forEach on a HTML collection BUT can on a Node List. Ugh... DOM unintuitiveness
   const menues = document.querySelectorAll(".more-menu");
   const notificationsBadge = document.getElementById("notifications-badge");
-  let numberActive = $('.glyphicon-bell.active').length;
+  let numberActive = document.querySelectorAll('.glyphicon-bell.active').length;
   document.addEventListener("click", event => {
     if (event.target.matches(".more-btn")) {
       menues.forEach(menu => {
