435f511606d7d811415ae86f93b8909e446a71eb
bmcilhenny <bmcilhen@gmail.com>
2019-07-18 15:28:36 -0400

    remove comments

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const main = function() {
-  // using querySelectorAll cause they're more modern, they return a Nodelist as opposed to a HTML collection like getElementsByClassName. You cannot call .forEach on a HTML collection BUT can on a Node List. Ugh... DOM unintuitiveness
   const menues = document.querySelectorAll(".more-menu");
-  const notificationsBadge = document.getElementById("notifications-badge");
+  const notificationsBadge = document.querySelector("#notifications-badge");
   let numberActive = document.querySelectorAll('.glyphicon-bell.active').length;
   document.addEventListener("click", event => {
     if (event.target.matches(".more-btn")) {
@@ -15,11 +14,6 @@ const main = function() {
     }
 
     if (even.target.matches(".notification")) {
-      // classList is not supported in internet explorer
-      // arr = element.className.split(" ");
-      // if (arr.indexOf(name) == -1) {
-      //   element.className += " " + name;
-      // }
       event.target.className += "active";
       notificationsBadge.textContent = numberActive;
     }
