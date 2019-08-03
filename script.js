dd9c8f3fef4e3608382b74c10a7cb87637e8808a
bmcilhenny <bmcilhen@gmail.com>
2019-08-03 15:50:15 -0400

    remove more-menu iteration

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const main = function() {
-  const menues = document.querySelectorAll(".more-menu");
   const notificationsBadge = document.querySelector("#notifications-badge");
   let numberActive = document.querySelectorAll('.glyphicon-bell.active').length;
   document.addEventListener("click", event => {
+  
     if (event.target.matches(".more-btn")) {
-      menues.forEach(menu => {
-        if (getComputedStyle(menu).display === "none") {
-          menu.style.setProperty("display", "block");
-        } else {
-          menu.style.setProperty("display", "none");
-        }
-      });
+      menuUnorderedList = event.target.nextElementSibling;
+      if (getComputedStyle(menuUnorderedList).display === "none") {
+        menuUnorderedList.style.setProperty("display", "block");
+      } else {
+        menuUnorderedList.style.setProperty("display", "none");
+      }
     }
 
     if (even.target.matches(".notification")) {
-      event.target.className += "active";
+      event.target.className += " active";
       notificationsBadge.textContent = numberActive;
     }
   });
